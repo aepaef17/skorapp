a8f7f3ec167abd5b36d542af9d71da75d3f14c00
Aep Saepudin <aepaef17@gmail.com>
2019-10-16 13:03:12 +0700

    Commit Kedua - Update fiture tombol

--- app/src/main/java/com/example/hellowworld2/MainActivity.java
+++ app/src/main/java/com/example/hellowworld2/MainActivity.java
@@ -10,6 +10,7 @@ public class MainActivity extends AppCompatActivity {
 
     private int skorA= 0;
     private int skorB= 0;
+    private int reset= 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,5 +23,45 @@ public class MainActivity extends AppCompatActivity {
         TextView textViewA = findViewById(R.id.skorA);
         textViewA.setText(String.valueOf(skorA));
     }
+
+    public void tambah2PointA(View view) {
+        skorA = skorA + 2;
+        TextView textViewA = findViewById(R.id.skorA);
+        textViewA.setText(String.valueOf(skorA));
+    }
+
+    public void tambah1PointA(View view) {
+        skorA = skorA + 1;
+        TextView textViewA = findViewById(R.id.skorA);
+        textViewA.setText(String.valueOf(skorA));
+    }
+
+    public void tambah3PoinB(View view) {
+        skorB = skorB + 3;
+        TextView textViewB = findViewById(R.id.skorB);
+        textViewB.setText(String.valueOf(skorB));
+    }
+
+    public void tambah2PointB(View view) {
+        skorB = skorB + 2;
+        TextView textViewB = findViewById(R.id.skorB);
+        textViewB.setText(String.valueOf(skorB));
+    }
+
+    public void tambah1PointB(View view) {
+        skorB = skorB + 1;
+        TextView textViewB = findViewById(R.id.skorB);
+        textViewB.setText(String.valueOf(skorB));
+    }
+
+    public void reset(View view){
+        reset=skorA=0;
+        reset=skorB=0;
+        TextView textViewA=findViewById(R.id.skorA);
+        TextView textViewB=findViewById(R.id.skorB);
+        textViewA.setText(String.valueOf(reset));
+        textViewB.setText(String.valueOf(reset));
+    }
+
 }
 
